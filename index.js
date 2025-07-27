4408160f06df1c62675950d547422c724ba66a62
EmonHossen10 <emonhossen1000@gmail.com>
2025-07-28 01:54:16 +0600

    your message here

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ async function run() {
     const menuCollection = client.db("quickDb").collection("menu");
     const reviewsCollection = client.db("quickDb").collection("reviews");
     const cartCollection = client.db("quickDb").collection("carts");
+    const paymentCollection = client.db("quickDb").collection("payments");
 
     // jwt related api
     app.post("/jwt", async (req, res) => {
@@ -203,19 +204,54 @@ async function run() {
     });
 
     // payment intent
+
     app.post("/create-payment-intent", async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
-      console.log(amount,'inside the backend')
+      if (amount < 50) {
+        return res.status(400).send({
+          error: "Amount must be at least $0.50",
+        });
+      }
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
 
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: "usd",
-        payment_method_types: ["card"],
-      });
-      res.send({
-        clientSecret: paymentIntent.client_secret,
-      });
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+      } catch (error) {
+        console.error("Stripe error:", error);
+        res.status(500).send({ error: error.message });
+      }
+    });
+    app.get("/payments/:email",verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
+
+      if (req.params.email !== req?.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      // carefully delete each item from the cart
+      console.log("payment info", payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({ paymentResult, deleteResult });
     });
 
     // Send a ping to confirm a successful connection
